5449d0755751059e0d002989c7ee9f507e363535
nzampe <nzampelunghe@gmail.com>
2018-12-08 01:12:41 -0300

    Interaccion

---
+++ app/src/main/java/com/zampegab/juegorazaspelajes/InteraccionCActivity.java
@@ -0,0 +1,14 @@
+package com.zampegab.juegorazaspelajes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class InteraccionCActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+    }
+
+}
