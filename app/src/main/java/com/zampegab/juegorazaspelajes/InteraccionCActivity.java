48438bb3e77f94b8fe1e892e3d4cf0a4bf0d29e6
nzampe <nzampelunghe@gmail.com>
2018-12-08 19:26:30 -0300

    Feedback sonido interaccion C

--- app/src/main/java/com/zampegab/juegorazaspelajes/InteraccionCActivity.java
+++ app/src/main/java/com/zampegab/juegorazaspelajes/InteraccionCActivity.java
@@ -2,13 +2,58 @@ package com.zampegab.juegorazaspelajes;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.media.MediaPlayer;
 
 public class InteraccionCActivity extends AppCompatActivity{
+
+    MediaPlayer mpResult;
+    ImageButton btnHorse1;
+    ImageButton btnHorse2;
+    ImageButton btnHorse3;
+    ImageButton btnHorse4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interaccion_c);
+
+        btnHorse1 = findViewById(R.id.horse1);
+        btnHorse1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mpResult = MediaPlayer.create(InteraccionCActivity.this, R.raw.incorrect);
+                mpResult.start();
+            }
+        });
+
+        btnHorse2 = findViewById(R.id.horse2);
+        btnHorse2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mpResult = MediaPlayer.create(InteraccionCActivity.this, R.raw.correct);
+                mpResult.start();
+            }
+        });
+
+        btnHorse3 = findViewById(R.id.horse3);
+        btnHorse3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mpResult = MediaPlayer.create(InteraccionCActivity.this, R.raw.incorrect);
+                mpResult.start();
+            }
+        });
+
+        btnHorse4 = findViewById(R.id.horse4);
+        btnHorse4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mpResult = MediaPlayer.create(InteraccionCActivity.this, R.raw.incorrect);
+                mpResult.start();
+            }
+        });
     }
 
 }
