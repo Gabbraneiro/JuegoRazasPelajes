cb50fd618059b481476d3436858cf1541c413cf1
nzampe <nzampelunghe@gmail.com>
2019-02-14 19:57:00 -0300

    asd

--- app/src/main/java/com/zampegab/juegorazaspelajes/MainActivity.java
+++ app/src/main/java/com/zampegab/juegorazaspelajes/MainActivity.java
@@ -1,5 +1,8 @@
 package com.zampegab.juegorazaspelajes;
 
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -8,7 +11,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,42 +21,89 @@ public class MainActivity extends AppCompatActivity {
     ImageButton btn_jugar;
     ImageButton btn_reconocimiento;
 
+    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<Caballo> caballo = Repositorio.getCaballosRandom(1);
-
         btn_config = findViewById(R.id.btn_conf);
         btn_reconocimiento = findViewById(R.id.btn_reconocimiento);
         btn_jugar = findViewById(R.id.btn_jugar);
 
-        btn_config.setOnClickListener(new OnClickListener(){
+        btn_config.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
                 startActivity(intent);
             }
         });
 
-        btn_reconocimiento.setOnClickListener(new OnClickListener(){
+        btn_reconocimiento.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v){
-                Intent intent = new Intent(MainActivity.this, GrillaActivity.class);
-                startActivity(intent);
+            public void onClick(View v) {
+                if (getModoReconocimiento() == "lista") {
+                    Intent intent = new Intent(MainActivity.this, ListaActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(MainActivity.this, GrillaActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
         btn_jugar.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, InteraccionBActivity.class);
-                startActivity(intent);
+                ArrayList<Caballo> caballos = Repositorio.getCaballosRandom(getCantCaballos());
+                HashMap modo_juego = getModoJuego();
+                if(modo_juego.get("interaccion") == "A"){
+                    Intent intent = new Intent(MainActivity.this, InteraccionAActivity.class);
+                    intent.putExtra("nivel", "1");
+                    startActivity(intent);
+                }
+                else if (modo_juego.get("interaccion") == "B"){
+                    Intent intent = new Intent(MainActivity.this, InteraccionBActivity.class);
+                    intent.putExtra("nivel", "2");
+                    startActivity(intent);
+                }
+                else {
+                    Intent intent = new Intent(MainActivity.this, InteraccionCActivity.class);
+                    intent.putExtra("nivel", "3");
+                    startActivity(intent);
+                }
             }
         });
     }
+
+    public Integer getCantCaballos() {
+        String pref_nivel = sharedPreferences.getString("nivel", "1");
+        if (pref_nivel == "1") {
+            return 2;
+        } else {
+            return 4;
+        }
+    }
+
+    public String getModoReconocimiento() {
+        String pref_nivel = sharedPreferences.getString("modo_reconocimiento", "1");
+        if (pref_nivel == "1") {
+            return "lista";
+        } else {
+            return "grilla";
+        }
+    }
+
+    public HashMap getModoJuego() {
+        HashMap<String,String> juego = new HashMap<String,String>();
+        String pref_interaccion = sharedPreferences.getString("modo_interaccion", "A");
+        String pref_minijuego = sharedPreferences.getString("minijuego", "1");
+        juego.put("interaccion", pref_interaccion);
+        juego.put("minijuego", pref_minijuego);
+        return juego;
+    }
+
 }
 
 
