d4b2f393aa648ae7c6948f03c6b897fa26965c07
nzampe <nzampelunghe@gmail.com>
2019-02-26 12:17:15 -0300

    Nueva clas: Minijuego

--- app/src/main/java/com/zampegab/juegorazaspelajes/SettingsActivity.java
+++ app/src/main/java/com/zampegab/juegorazaspelajes/SettingsActivity.java
@@ -1,31 +1,12 @@
 package com.zampegab.juegorazaspelajes;
 
-import android.content.SharedPreferences;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 
-public class SettingsActivity extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
-
-        /*final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-
-        if(Minijuego.getMinijuego().getActual() != 3){
-            Preference preference = findViewById("modo_interaccion");
-
-            //sharedPreferences.
-        }
-
-    }
-
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if(key.equals())
-    }*/
     }
 }
