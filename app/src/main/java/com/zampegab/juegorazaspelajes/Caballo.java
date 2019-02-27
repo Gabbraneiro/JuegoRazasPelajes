8fa05bb96cfb067efa92d48d4c55456d0e9fbd61
Gabriel Brañeiro <gabbraneiro@gmail.com>
2019-02-27 10:18:18 -0300

    ffasd

--- app/src/main/java/com/zampegab/juegorazaspelajes/Caballo.java
+++ app/src/main/java/com/zampegab/juegorazaspelajes/Caballo.java
@@ -24,67 +24,39 @@ public class Caballo implements Serializable {
         return id;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
     public String getRaza() {
         return raza;
     }
 
-    public void setRaza(String raza) {
-        this.raza = raza;
-    }
 
     public String getPelaje() {
         return pelaje;
     }
 
-    public void setPelaje(String pelaje) {
-        this.pelaje = pelaje;
-    }
 
     public String getDescripcion() {
         return descripcion;
     }
 
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
 
     public int getImg() {
         return img;
     }
 
-    public void setImg(int img) {
-        this.img = img;
-    }
 
     public int getAudio_pelaje() {
         return audio_pelaje;
     }
 
-    public void setAudio_pelaje(int audio_pelaje) {
-        this.audio_pelaje = audio_pelaje;
-    }
 
     public int getAudio_raza() {
         return audio_raza;
     }
 
-    public void setAudio_raza(int audio_raza) {
-        this.audio_raza = audio_raza;
-    }
 
     public int getAudio_raza_pelaje() {
         return audio_raza_pelaje;
     }
 
-    public void setAudio_raza_pelaje(int audio_raza_pelaje) {
-        this.audio_raza_pelaje = audio_raza_pelaje;
-    }
 }
